cc947774083b374693d6b63613e24e14f87e7f45
HZ-2000 <delta-z@outlook.com>
2022-06-22 12:57:01 -0700

    Added TopNav and router with new routes

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -1,10 +1,19 @@
 import styles from './Home.module.css';
+import { TopNav, SideNav, Contact, Projects } from '..';
 
 export const Home: React.FC = () => {
     return(
-        <div className={styles.main}>
-            <h1>This is a test page lol</h1>
-            <h2>Welcome!</h2>
+        <div>
+            <TopNav />
+            <SideNav />
+            <section id="home">
+                <div className={styles.main}>
+                    <h1>This is a test page lol</h1>
+                    <h2>Welcome!</h2>
+                </div>
+            </section>
+            <section id="projects"><Projects /></section>
+            <section id="contact"><Contact /></section>
         </div>
     )
 }
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,16 @@
 import ReactDOM from 'react-dom';
-import { App } from './components';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home } from './components';
 import './index.css';
 
 ReactDOM.render(
-  <App />
+  <Router>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/about' element={<Home />}/>
+      <Route path='/projects' element={<Home />}/>
+    </Routes>
+  </Router>
   ,
   document.getElementById('root')
 );

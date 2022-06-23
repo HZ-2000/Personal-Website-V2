47912524284a924cd3b18bf218156218646e2959
HZ-2000 <delta-z@outlook.com>
2022-06-23 08:09:06 -0700

    darkened background image and fixed width problem

--- client/src/components/Contact/Contact.tsx
+++ client/src/components/Contact/Contact.tsx
@@ -1,8 +1,10 @@
+import { TopNav } from '..';
 import styles from './Contact.module.css';
 
 export const Contact: React.FC = () => {
     return (
         <div className={styles.main}>
+            <TopNav />
             <h1>Contact</h1>
         </div>
     )

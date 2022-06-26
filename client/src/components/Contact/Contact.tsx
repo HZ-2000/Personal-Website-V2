d184d846af0a63c9da416aca5fedb88721df8d48
HZ-2000 <delta-z@outlook.com>
2022-06-25 21:50:26 -0700

    Added more to contact page!

--- client/src/components/Contact/Contact.tsx
+++ client/src/components/Contact/Contact.tsx
@@ -1,5 +1,6 @@
 import { TopNav } from '..';
 import styles from './Contact.module.css';
+import resume from '../../assets/Screenshot from 2021-10-19 22-04-03.png';
 
 export const Contact: React.FC = () => {
     document.title = 'ZH | Contact';
@@ -16,7 +17,10 @@ export const Contact: React.FC = () => {
                 </div>
                 <div className={styles.ResumeSection}>
                     <h1>Resume:</h1>
-                    <a className={styles.LinkWrapper} href=''>link</a>
+                    <div>
+                        <a className={styles.LinkWrapper} href=''>link</a> 
+                    </div>
+                    <img src={resume} alt='resume'/>
                 </div>
             </div>
         </div>

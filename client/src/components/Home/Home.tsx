50f0f6869a2ae1eb529680d246fcd3d7d03e220b
HZ-2000 <delta-z@outlook.com>
2022-06-26 20:21:53 -0700

    added animations and profile pic

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -10,8 +10,8 @@ export const Home: React.FC = () => {
             <SideNav />
             <section id="home">
                 <div className={styles.intro}>
-                    <p className={styles.introMainText}>Viva La Vida</p>
-                    <p>Long Live Life</p>
+                    <p className={styles.introMainText}>"Cogito, ergo sum"</p>
+                    <p>- I think, therfore I am</p>
                 </div>
             </section>
             <section id="projects">
@@ -19,11 +19,11 @@ export const Home: React.FC = () => {
                     <p className={styles.title}>About</p>
                     <div className={styles.projBox}>
                         <div className={styles.gridItem}>
-                            <p className={styles.gridItemTitle}>Picture</p>
+                            <div className={styles.portrait}/>
                         </div>
                         <div style={{"width": "4%"}}/>
                         <div className={styles.gridItem}>
-                            <p className={styles.gridItemTitle}>Hi, Im Zach!</p>
+                            <p className={styles.gridItemTitle}>Welcome, Im Zach!</p>
                             <p>
                                 I am currently a intern at SEL and enjoy the challenges of learning new technologies and ideas,
                                  whilst simultaneously getting to use them in the development process! outside of work I enjoy cycling
@@ -39,17 +39,17 @@ export const Home: React.FC = () => {
                     <p className={styles.title}>Featured Projects</p>
                     <div className={styles.projBox}>
                         <div className={styles.gridItem}>
-                            <p className={styles.gridItemTitle}>Machine Learning Demo</p>
+                            <p className={styles.gridItemTitle}>Personal Website V2</p>
                             <p>this is a brief description of the project and will be displayed above the link to my github where the project is!</p>
-                            <p>Languages: Python</p>
+                            <p>Languages: React</p>
                             <a href='/' className={styles.LinkWrapper}>link</a>
                         </div>
                         <div style={{"width": "4%"}}/>
                         <div className={styles.gridItem}>
-                            <p className={styles.gridItemTitle}>Fair Home Pro</p>
+                            <p className={styles.gridItemTitle}>Machine Learning Demo</p>
                             <p>this is a brief description of the project and will be displayed above the link to my github where the project is!</p>
-                            <p>Languages: </p>
-                            <a href='/' className={styles.LinkWrapper}>link</a>
+                            <p>Languages: Python</p>
+                            <a href='https://github.com/HZ-2000/Machine-Learning-Demo' className={styles.LinkWrapper}>link</a>
                         </div>
                     </div>
                 </div>

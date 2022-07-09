3c5806e206e64c60cc5248d03e2a16633f0d1302
HZ-2000 <delta-z@outlook.com>
2022-07-09 15:51:50 -0700

    Animations added, Projects page added

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -1,4 +1,5 @@
 import styles from './Home.module.css';
+import { DiGithubBadge, DiReact, DiPython } from 'react-icons/di';
 import { TopNav, SideNav } from '..';
 
 export const Home: React.FC = () => {
@@ -40,16 +41,21 @@ export const Home: React.FC = () => {
                     <div className={styles.projBox}>
                         <div className={styles.gridItem}>
                             <p className={styles.gridItemTitle}>Personal Website V2</p>
-                            <p>this is a brief description of the project and will be displayed above the link to my github where the project is!</p>
-                            <p>Languages: React</p>
-                            <a href='/' className={styles.LinkWrapper}>link</a>
+                            <p>
+                                This is the website you are currently visiting! It is a great test bed for me to learn 
+                                new technologies or just have fun trying to make interesting designs. The current iteration 
+                                is very basic however in the future I am planning on adding to it extensivley with games, 
+                                weird tools, and fun side projects.
+                            </p>
+                            <p>Languages: <DiReact style={{verticalAlign: "middle"}}/>React</p>
+                            <a href='https://github.com/HZ-2000/Machine-Learning-Demo' className={styles.LinkWrapper}><DiGithubBadge style={{verticalAlign: "middle"}}/>GitHub</a>
                         </div>
                         <div style={{"width": "4%"}}/>
                         <div className={styles.gridItem}>
                             <p className={styles.gridItemTitle}>Machine Learning Demo</p>
                             <p>this is a brief description of the project and will be displayed above the link to my github where the project is!</p>
-                            <p>Languages: Python</p>
-                            <a href='https://github.com/HZ-2000/Machine-Learning-Demo' className={styles.LinkWrapper}>link</a>
+                            <p>Languages: <DiPython style={{verticalAlign: "middle"}}/>Python</p>
+                            <a href='https://github.com/HZ-2000/Machine-Learning-Demo' className={styles.LinkWrapper}><DiGithubBadge  style={{verticalAlign: "middle"}}/>GitHub</a>
                         </div>
                     </div>
                 </div>
--- client/src/components/Projects/Projects.tsx
+++ client/src/components/Projects/Projects.tsx
@@ -1,4 +1,5 @@
 import { TopNav } from '..';
+import { DiGithubBadge, DiReact } from 'react-icons/di';
 import styles from './Projects.module.css';
 
 export const Projects: React.FC = () => {
@@ -7,8 +8,52 @@ export const Projects: React.FC = () => {
     return (
         <div className={styles.main}>
             <TopNav />
-            <div className={styles.title}>
-                <h1>Projects</h1>
+            <h1 className={styles.title}>Projects</h1>
+            <div className={styles.ProjectGrid}>
+                <div className={styles.Project}>
+                    <h1>Personal Website V2</h1>
+                    <p>
+                        This is the website you are currently visiting! It is a great test bed for me to learn 
+                        new technologies or just have fun trying to make interesting designs. The current iteration 
+                        is very basic however in the future I am planning on adding to it extensivley with games, 
+                        weird tools, and fun side projects.
+                    </p>
+                    <p>Languages: <DiReact style={{verticalAlign: "middle"}}/>React</p>
+                    <a href='https://github.com/HZ-2000/Machine-Learning-Demo' className={styles.LinkWrapper}><DiGithubBadge style={{verticalAlign: "middle"}}/>GitHub</a>
+                </div>
+                <div className={styles.Project}>
+                    <h1>Personal Website V2</h1>
+                    <p>
+                        This is the website you are currently visiting! It is a great test bed for me to learn 
+                        new technologies or just have fun trying to make interesting designs. The current iteration 
+                        is very basic however in the future I am planning on adding to it extensivley with games, 
+                        weird tools, and fun side projects.
+                    </p>
+                    <p>Languages: <DiReact style={{verticalAlign: "middle"}}/>React</p>
+                    <a href='https://github.com/HZ-2000/Machine-Learning-Demo' className={styles.LinkWrapper}><DiGithubBadge style={{verticalAlign: "middle"}}/>GitHub</a>
+                </div>
+                <div className={styles.Project}>
+                    <h1>Personal Website V2</h1>
+                    <p>
+                        This is the website you are currently visiting! It is a great test bed for me to learn 
+                        new technologies or just have fun trying to make interesting designs. The current iteration 
+                        is very basic however in the future I am planning on adding to it extensivley with games, 
+                        weird tools, and fun side projects.
+                    </p>
+                    <p>Languages: <DiReact style={{verticalAlign: "middle"}}/>React</p>
+                    <a href='https://github.com/HZ-2000/Machine-Learning-Demo' className={styles.LinkWrapper}><DiGithubBadge style={{verticalAlign: "middle"}}/>GitHub</a>
+                </div>
+                <div className={styles.Project}>
+                    <h1>Personal Website V2</h1>
+                    <p>
+                        This is the website you are currently visiting! It is a great test bed for me to learn 
+                        new technologies or just have fun trying to make interesting designs. The current iteration 
+                        is very basic however in the future I am planning on adding to it extensivley with games, 
+                        weird tools, and fun side projects.
+                    </p>
+                    <p>Languages: <DiReact style={{verticalAlign: "middle"}}/>React</p>
+                    <a href='https://github.com/HZ-2000/Machine-Learning-Demo' className={styles.LinkWrapper}><DiGithubBadge style={{verticalAlign: "middle"}}/>GitHub</a>
+                </div>
             </div>
         </div>
     )

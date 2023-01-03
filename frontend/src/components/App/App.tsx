0b926175d3ff27eacc505d7a717420276bf0d099
HZ-2000 <delta-z@outlook.com>
2023-01-02 20:15:10 -0800

    Added custom themeing

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,16 +1,38 @@
-import { Container, Box } from '@mui/system';
+import { Box } from '@mui/system';
 import { Outlet } from 'react-router-dom';
 import React from 'react';
 import { ABar } from '../';
+import { createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@emotion/react';
+
+const theme = createTheme({
+    palette: {
+        mode: "light",
+        primary: {
+            light: '#718792',
+            main: '#455a64',
+            dark: '#1c313a',
+            contrastText: '#fff',
+        },
+        secondary: {
+            light: '#87ffff',
+            main: '#4ccfe0',
+            dark: '#009eae',
+            contrastText: '#000',
+        },
+    },
+});
 
 
 export const App: React.FC = () => {
     return (
         <React.Fragment>
-            <ABar />
-            <Box>
-                <Outlet />
-            </Box>
+            <ThemeProvider theme={theme}>
+                <ABar />
+                <Box>
+                    <Outlet />
+                </Box>
+            </ThemeProvider>
         </React.Fragment>
     )
 }

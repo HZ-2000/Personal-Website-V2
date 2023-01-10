120ef7aff1ec83b278d0cc32dc9eb5dfe4266ec4
HZ-2000 <delta-z@outlook.com>
2023-01-09 20:30:38 -0800

    Added images and content to pages

---
+++ frontend/src/common/common.tsx
@@ -0,0 +1,40 @@
+import tree from '../assets/img/matthew-smith-rFBA42UFpLs-unsplash.jpg'
+import pong_paddle from '../assets/img/nicholas-ismael-martinez-LsmKRNXnyyA-unsplash.jpg'
+import house from '../assets/img/luke-stackpoole-eWqOgJ-lfiI-unsplash.jpg'
+import wing from '../assets/img/fransiskus-filbert-mangundap-LnrNbu-DeIc-unsplash.jpg'
+
+
+export const personal_website = {
+    name: "Personal Website",
+    description: "Roboto is a cool guy, I mean look at him, he is made out of legos and he lookin up. And the color pallete is spot on.",
+    image: tree,
+    link: ""
+}
+
+export const prandtld_system = {
+    name: "Prandtl-D System",
+    description: "Roboto is a cool guy, I mean look at him, he is made out of legos and he lookin up. And the color pallete is spot on.",
+    image: wing,
+    link: ""
+}
+
+export const fair_home_pro = {
+    name: "Fair Home Pro",
+    description: "Roboto is a cool guy, I mean look at him, he is made out of legos and he lookin up. And the color pallete is spot on.",
+    image: house,
+    link: ""
+}
+
+export const pong = {
+    name: "Pong",
+    description: "Roboto is a cool guy, I mean look at him, he is made out of legos and he lookin up. And the color pallete is spot on.",
+    image: pong_paddle,
+    link: "" 
+}
+
+export const projects = [
+    personal_website,
+    prandtld_system,
+    fair_home_pro,
+    pong
+]

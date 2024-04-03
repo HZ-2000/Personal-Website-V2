9487622251504fbd65837731e0cfe3c5f1e4dcfe
HZ-2000 <delta.ztx@gmail.com>
2024-04-03 21:30:42 +0000

    fix(nextjs): cleanup

--- src/common/common.tsx
+++ src/common/common.tsx
@@ -1,32 +1,26 @@
-import tree from '../assets/img/matthew-smith-rFBA42UFpLs-unsplash.jpg'
-import pong_paddle from '../assets/img/nicholas-ismael-martinez-LsmKRNXnyyA-unsplash.jpg'
-import house from '../assets/img/luke-stackpoole-eWqOgJ-lfiI-unsplash.jpg'
-import tomato from '../assets/img/blackieshoot-Y8HRUhP-rkM-unsplash.jpg'
-
-
 export const projects = [
     {
         name: "Personal Website",
         description: "The very site you are visiting! This is a place where i learn and test new things like React18, MUI, and hopefully github actions at some point.",
-        image: tree,
+        image: '../assets/img/matthew-smith-rFBA42UFpLs-unsplash.jpg',
         link: "https://github.com/HZ-2000/Personal-Website-V2"
     },
     {
         name: "Pomodoro Timer",
         description: "A web page with a journal style log and a pomodoro timer to help with productivity.",
-        image: tomato,
+        image: '../../assets/img/blackieshoot-Y8HRUhP-rkM-unsplash.jpg',
         link: "https://github.com/HZ-2000"
     },
     {
         name: "Fair Home Pro",
         description: "A Home imporvment site that connects users to different contactors for bids on home improvment projects.",
-        image: house,
+        image: '../assets/img/luke-stackpoole-eWqOgJ-lfiI-unsplash.jpg',
         link: "https://github.com/HZ-2000/FairHomePro"
     },
     {
         name: "Pong",
         description: "A remake of a classic game as a gateway into Unity. This project was also used in a presentation given in a class called Software Engineering, to introduce Unity's testing system.",
-        image: pong_paddle,
+        image: '../assets/img/nicholas-ismael-martinez-LsmKRNXnyyA-unsplash.jpg',
         link: "https://github.com/HZ-2000/Pong" 
     }
 ]
